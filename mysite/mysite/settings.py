5cad1a947941c12f88328bd82fb34f4364c06652
뀨ㅇㅇ <ysh02241@hanmail.net>
2018-11-20 15:30:40 +0900

    ff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,18 +20,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_i46(^+&lfvgx^5n!@0*4hg@umuz_*29%m^w%i7_1(+se=vjm='
+SECRET_KEY = ')c1usthx3zrqw((&h)te#kviba4(4lidkg2wd4e1s$@l(m$&&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com','localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
